4e47a3109fc7e39dfb0dce4daa0b8eef356a79ea
aurelioprovedo <aurelioprovedo@gmail.com>
2019-03-09 13:34:27 +0100

    Improving joint-based soft bodies

---
+++ Project/Assets/_WIP/Soft Body Test/SimpleMoveSoftBodyByForce.cs
@@ -0,0 +1,58 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+
+public class SimpleMoveSoftBodyByForce : MonoBehaviour
+{
+    [Required]
+    public SoftBodyCreator SoftBody;
+
+    public float Force = 10;
+    public ForceMode Mode = ForceMode.Acceleration;
+    public float Torque = 10;
+    //public bool AffectedByMass = false;
+    //public float MaxSpeed = 1;
+    public float RunMultiplier = 2;
+
+
+    private Vector3 _inputAccel;
+    private Vector3 _inputTorque;
+
+
+    void Update()
+    {
+        var horiz = Input.GetAxis("Horizontal");
+        float vert = (Input.GetKey(KeyCode.Q) ? -1 : 0) + (Input.GetKey(KeyCode.E) ? 1 : 0);
+        var forward = Input.GetAxis("Vertical");
+        var run = Input.GetButton("Fire3");
+        var rotate = Input.GetButton("Fire1");
+
+        var multip = run ? RunMultiplier : 1;
+
+        _inputAccel = rotate ? Vector3.zero : RunMultiplier * new Vector3(horiz, vert, forward);
+        _inputTorque = !rotate ? Vector3.zero : RunMultiplier * new Vector3(horiz, vert, forward);
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (!SoftBody || !SoftBody.IsCreated)
+            return;
+
+        var accel = _inputAccel * Force;
+        var torque = _inputTorque * Torque;
+
+        for (int i = 0; i < SoftBody.Count; i++)
+        {
+            var particle = SoftBody.Get(i);
+
+            if (particle)
+            {
+                particle.AddForce(accel, Mode);
+                particle.AddTorque(_inputTorque);
+            }
+
+            //TODO: add torque
+        }
+    }
+}
--- Project/Assets/_WIP/Soft Body Test/SoftBodyCreator.cs
+++ Project/Assets/_WIP/Soft Body Test/SoftBodyCreator.cs
@@ -1,46 +1,44 @@
-﻿//using Sirenix.OdinInspector;
+﻿using NaughtyAttributes;
 using UnityEngine;
-
+using UnityEngine.Serialization;
 
 public class SoftBodyCreator : MonoBehaviour
 {
-    public enum JointType
-    {
-        Spring,
-        Fixed,
-        Configurable
-    }
-
-
     //[Required]
-    [Header("Particle")]
-    public Rigidbody Prefab;
+    // TODO: Must be scene object
+    [BoxGroup("Original Particle - NOT PREFAB, MUST BE IN SCENE")]
+    public Rigidbody OriginalParticle;
 
-    [Header("Shape")]
-    public int Width = 5;
-    public int Height = 2;
-    public int Depth = 3;
-    public float Offset = 1;
-    public bool OmitCorners = true;
+    [BoxGroup("Shape")] [MinValue(.0001f)] public float ColliderDiameter = 0.03f;
+    [BoxGroup("Shape")] [MinValue(1)] public int CountX = 5;
+    [BoxGroup("Shape")] [MinValue(1)] public int CountY = 2;
+    [BoxGroup("Shape")] [MinValue(1)] public int CountZ = 3;
 
-    [Header("Joints")]
-    public bool ConnectDiagonals = false;
-    public JointType Type;
-    public bool EnablePreprocessing = true;
-    //[ShowIf("Type", JointType.Spring)]
-    public float Spring = 1000;
-    //[ShowIf("Type", JointType.Spring)]
-    public float Damper = 10;
-    public float Limit = 0.05f;
-    public float AngularLimit = 30f;
-    public float Bounciness = 0;
-    public float ContactDistance = 0.001f;
-    public float RotationSpring = 500;
-    public float BreakForce = Mathf.Infinity;
-    public float BreakTorque = Mathf.Infinity;
+    [Tooltip("Distance between particles. It is multiplied by the ColliderDiameter. Leave to 1 to have touching colliders on creation. IMPORTANT: LinearLimits are relative to this.")]
+    [BoxGroup("Shape")] [MinValue(0)]  public float DistanceMultiplier = 1;
 
-    [Space(60)]
-    public bool DEBUG_RECREATE_BUTTON;
+    [BoxGroup("Shape")] public bool OmitCorners = true;
+
+
+    [BoxGroup("Joints")] public bool ConnectDiagonals = false;
+    [BoxGroup("Joints")] public bool EnablePreprocessing = true;
+    [BoxGroup("Joints")] [MinValue(0)] public float LinearSpring = 200;
+    [BoxGroup("Joints")] [MinValue(0)] public float LinearDamper = 10;
+
+    [Tooltip("This is multiplied by the ColliderDiameter and the DistanceMultiplier.")]
+    [BoxGroup("Joints")] [MinValue(0)] public float LinearLimitRelative = .9f;
+
+    [BoxGroup("Joints")] [MinValue(0)] public float AngularSpring = 2;
+    [BoxGroup("Joints")] [MinValue(0)] public float AngularDamper = .05f;
+    [BoxGroup("Joints")] [MinValue(0)] public float AngularLimit = 45f;
+
+    [Tooltip("This is multiplied by the ColliderDiameter and the DistanceMultiplier.")]
+    [BoxGroup("Joints")] [MinValue(0)] public float HardLinearLimitRelative = 1.333f;
+
+    [BoxGroup("Joints")] [MinValue(0)] public float HardAngularLimit = 60f;
+    [BoxGroup("Joints")] [MinValue(0.01f)] public float BreakForce = Mathf.Infinity;
+    [BoxGroup("Joints")] [MinValue(0.01f)] public float BreakTorque = Mathf.Infinity;
+
 
     [SerializeField]
     [HideInInspector]
@@ -50,23 +48,56 @@ public class SoftBodyCreator : MonoBehaviour
     public bool IsCreated { get => _particles != null && _particles.Length > 0; }
 
 
+    public float DistanceBetweenParticles { get => ColliderDiameter * DistanceMultiplier; }
+
+
     private void Start()
     {
         if (!IsCreated)
-            Create();
+            Create();        
     }
 
 
-    private void OnValidate()
+#if UNITY_EDITOR
+    public bool DEBUG_LiveUpdate = false;
+    [MinValue(0)] public int DEBUG_UpdateFrameSkip = 60;
+    private int _updatesToSkip = 0;
+    private void FixedUpdate()
     {
-        if (DEBUG_RECREATE_BUTTON)
-            Recreate();
-        DEBUG_RECREATE_BUTTON = false;
+        if (!DEBUG_LiveUpdate)
+            return;
+
+        _updatesToSkip--;
+        if (_updatesToSkip >= 0)
+            return;
+        _updatesToSkip = DEBUG_UpdateFrameSkip;
+
+        ResetAllJointValues();
+    }
+#endif
+
+
+    [Button("Reset All Joint Values")]
+    [ContextMenu("Reset All Joint Values")]
+    public void ResetAllJointValues()
+    {
+        if (!IsCreated)
+            return;
+
+        foreach (var p in _particles)
+        {
+            if (!p)
+                continue;
+
+            foreach (var j in p.GetComponents<ConfigurableJoint>())
+                SetJointValues(j);
+            p.WakeUp();
+        }
     }
 
 
+    [Button]
     [ContextMenu("Recreate")]
-    //[Button]
     public void Recreate()
     {
         Destroy();
@@ -81,7 +112,7 @@ public class SoftBodyCreator : MonoBehaviour
 
         foreach (var rb in _particles)
         {
-            if (!rb)
+            if (!rb || rb == OriginalParticle)
                 continue;
 
             if (Application.isPlaying)
@@ -96,64 +127,84 @@ public class SoftBodyCreator : MonoBehaviour
 
     public void Create()
     {
-        _particles = new Rigidbody[Width * Height * Depth];
+        // TODO: optimize this method's code, especially the loops.
 
-        // Create and position all particles.
-        for (int i = 0; i < Width; i++)
+        if (CountX * CountY * CountZ == 0)
+            return;
+
+        _particles = new Rigidbody[CountX * CountY * CountZ];
+
+        // Update the original particle and set it as the center one.
+        var radius = ColliderDiameter * .5f;
+        foreach (var j in OriginalParticle.GetComponents<ConfigurableJoint>())
+            DestroyImmediate(j, false);
+        var col = OriginalParticle.GetComponent<SphereCollider>();
+        if (!col)
+            col = OriginalParticle.gameObject.AddComponent<SphereCollider>();
+        col.radius = radius;
+        var centerIndex = Index(CountX / 2, CountY / 2, CountZ / 2);
+        _particles[centerIndex] = OriginalParticle;
+
+        // Create and position all other particles.
+        var origin = OriginalParticle.position;
+        var dist = DistanceBetweenParticles;
+        var parent = OriginalParticle.transform.parent;
+        var halfSize = .5f * dist * new Vector3(CountX, CountY, CountZ) - new Vector3(radius, radius, radius);
+        for (int i = 0; i < CountX; i++)
         {
-            for (int j = 0; j < Height; j++)
+            for (int j = 0; j < CountY; j++)
             {
-                for (int k = 0; k < Depth; k++)
+                for (int k = 0; k < CountZ; k++)
                 {
-                    if (OmitCorners && (i == 0 || i == Width - 1) && (j == 0 || j == Height - 1) && (k == 0 || k == Depth - 1))
+                    if (OmitCorners && IsCorner(i, j, k))
                         continue;
 
-                    var localPos = new Vector3(i * Offset, j * Offset, k * Offset);
-                    var rb = Instantiate<Rigidbody>(Prefab, transform);
-                    rb.transform.localPosition = localPos;
-                    _particles[Index(i, j, k)] = rb;
+                    var index = Index(i, j, k);
+                    if (index == centerIndex)
+                        continue;
+
+                    //var localPos = -halfSize + dist * new Vector3(i, j, k);
+                    var pos = origin - halfSize + dist * new Vector3(i, j, k);
+                    var rb = Instantiate<Rigidbody>(OriginalParticle, parent);
+                    //rb.transform.localPosition = localPos;
+                    rb.transform.position = pos;
+                    //rb.position = pos;
+                    Set(index, rb);
                 }
             }
         }
 
         // Connect particles via joints.
-        JointConnectDelegate connectDelegate;
-        if (Type == JointType.Spring)
-            connectDelegate = new JointConnectDelegate(ConnectSpring);
-        else if (Type == JointType.Configurable)
-            connectDelegate = new JointConnectDelegate(ConnectConfigurable);
-        else
-            connectDelegate = new JointConnectDelegate(ConnectFixed);
-        for (int i = 0; i < Width; i++)
+        for (int i = 0; i < CountX; i++)
         {
-            for (int j = 0; j < Height; j++)
+            for (int j = 0; j < CountY; j++)
             {
-                for (int k = 0; k < Depth; k++)
+                for (int k = 0; k < CountZ; k++)
                 {
-                    if (OmitCorners && (i == 0 || i == Width - 1) && (j == 0 || j == Height - 1) && (k == 0 || k == Depth - 1))
+                    if (OmitCorners && IsCorner(i, j, k))
                         continue;
 
                     var rb = Get(i, j, k);
 
-                    if (i < Width - 1)
-                        connectDelegate(rb, Get(i + 1, j, k));
+                    if (i < CountX - 1)
+                        ConnectConfigurable(rb, Get(i + 1, j, k));
 
-                    if (j < Height - 1)
-                        connectDelegate(rb, Get(i, j + 1, k));
+                    if (j < CountY - 1)
+                        ConnectConfigurable(rb, Get(i, j + 1, k));
 
-                    if (k < Depth - 1)
-                        connectDelegate(rb, Get(i, j, k + 1));
+                    if (k < CountZ - 1)
+                        ConnectConfigurable(rb, Get(i, j, k + 1));
 
                     if (ConnectDiagonals)
                     {
-                        if (i < Width && j < Height)
-                            connectDelegate(rb, Get(i + 1, j + 1, k));
+                        if (i < CountX - 1 && j < CountY - 1)
+                            ConnectConfigurable(rb, Get(i + 1, j + 1, k));
 
-                        if (i < Width && k < Depth)
-                            connectDelegate(rb, Get(i + 1, j, k + 1));
+                        if (i < CountX - 1 && k < CountZ - 1)
+                            ConnectConfigurable(rb, Get(i + 1, j, k + 1));
 
-                        if (j < Height && k < Depth)
-                            connectDelegate(rb, Get(i, j + 1, k + 1));
+                        if (j < CountY - 1 && k < CountZ - 1)
+                            ConnectConfigurable(rb, Get(i, j + 1, k + 1));
 
 
                         /*if (i < Dimension - 1 && j < Dimension - 1)
@@ -169,44 +220,50 @@ public class SoftBodyCreator : MonoBehaviour
     }
 
 
-    private Rigidbody Get(int i, int j, int k)
+    // TODO: make these with IEnumerable or something
+    #region Array
+
+    public int Count { get => _particles == null ? 0 : _particles.Length; }
+
+
+    // i -> Width, j -> Height, k -> Depth
+    public Rigidbody Get(int i, int j, int k)
     {
         return _particles[Index(i, j, k)];
     }
 
 
+    // One-dimensional index getter
+    public Rigidbody Get(int index)
+    {
+        return _particles[index];
+    }
+
+
+    private void Set(int i, int j, int k, Rigidbody rb)
+    {
+        _particles[Index(i, j, k)] = rb;
+    }
+
+
+    private void Set(int index, Rigidbody rb)
+    {
+        _particles[index] = rb;
+    }
+
+
     private int Index(int i, int j, int k)
     {
-        return i * Height * Depth + j * Depth + k;
+        return i * CountY * CountZ + j * CountZ + k;
     }
 
 
-    private delegate Joint JointConnectDelegate(Rigidbody a, Rigidbody b);
-
-    private Joint ConnectFixed(Rigidbody a, Rigidbody b)
+    private bool IsCorner(int i, int j, int k)
     {
-        var joint = a.gameObject.AddComponent<FixedJoint>();
-        joint.connectedBody = b;
-        joint.enablePreprocessing = EnablePreprocessing;
-        return joint;
+        return (i == 0 || i == CountX - 1) && (j == 0 || j == CountY - 1) && (k == 0 || k == CountZ - 1);
     }
 
-    
-    private Joint ConnectSpring(Rigidbody a, Rigidbody b)
-    {
-        var joint = a.gameObject.AddComponent<SpringJoint>();
-        joint.connectedBody = b;
-        joint.spring = Spring;
-        joint.damper = Damper;
-        joint.enablePreprocessing = EnablePreprocessing;
-        return joint;
-    }
-
-
-    private bool Is1(float x)
-    {
-        return Mathf.Approximately(1, Mathf.Abs(x));
-    }
+    #endregion Array
 
 
     private Joint ConnectConfigurable(Rigidbody a, Rigidbody b)
@@ -216,57 +273,89 @@ public class SoftBodyCreator : MonoBehaviour
 
         var joint = a.gameObject.AddComponent<ConfigurableJoint>();
         joint.connectedBody = b;
-
-        joint.enablePreprocessing = EnablePreprocessing;
-        //joint.autoConfigureConnectedAnchor = true;
         joint.configuredInWorldSpace = true;
+        //joint.autoConfigureConnectedAnchor = true;
 
-        var aPos = a.position;
-        var bPos = b.position;
-        var AtoB = bPos - aPos;
-        var dir = AtoB.normalized;
+        var dir = (b.position - a.position).normalized;
         joint.axis = dir;
-
-        // It doesn't matter where the secondary axis points as long as it is different from the main axis.
-        //var cross1 = Vector3.Cross(dir, Vector3.right);
-        //var cross2 = Vector3.Cross(dir, cross1);
-        //joint.secondaryAxis = cross2;
-        //joint.secondaryAxis = Quaternion.AngleAxis(90, dir) * Vector3.forward;
         joint.secondaryAxis = Quaternion.LookRotation(dir) * Vector3.right;
-        joint.secondaryAxis.Normalize();
 
-        if (joint.secondaryAxis.Equals(dir))
-            Debug.LogError("MIERDA");
-
-        joint.xMotion = ConfigurableJointMotion.Limited;
-        //joint.yMotion = ConfigurableJointMotion.Locked;
-        //joint.zMotion = ConfigurableJointMotion.Locked;
-        joint.yMotion = ConfigurableJointMotion.Limited;
-        joint.zMotion = ConfigurableJointMotion.Limited;
-        joint.angularXMotion = ConfigurableJointMotion.Locked;
-        joint.angularYMotion = ConfigurableJointMotion.Limited;
-        joint.angularZMotion = ConfigurableJointMotion.Limited;
-
-        joint.linearLimit = new SoftJointLimit() { limit = Limit };
-        joint.angularYLimit = new SoftJointLimit() { limit = AngularLimit };
-        joint.angularZLimit = new SoftJointLimit() { limit = AngularLimit };
-
-        joint.xDrive = new JointDrive() { positionSpring = Spring, positionDamper = Damper, maximumForce = Mathf.Infinity };
-        joint.angularYZDrive = new JointDrive() { positionSpring = RotationSpring, positionDamper = Damper, maximumForce = Mathf.Infinity };
-
-        joint.breakForce = BreakForce;
-        joint.breakTorque = BreakTorque;
-
-
-        //HARDCODING
-        joint.projectionMode = JointProjectionMode.PositionAndRotation;
-        joint.projectionDistance = Limit;
-        joint.projectionAngle = AngularLimit;
-
-
+        SetJointValues(joint);
 
 Debug.Log(" QUE NO CHOQUEN ENTRE LAS PARTICULAS!! MOSTARAR UN MENSAJE DE ERROR SI OCURRE!!");
 
         return joint;
     }
+
+
+    private void SetJointValues(ConfigurableJoint joint)
+    {
+        joint.enablePreprocessing = EnablePreprocessing;
+
+        joint.xMotion = ConfigurableJointMotion.Limited;
+        joint.yMotion = ConfigurableJointMotion.Limited;
+        joint.zMotion = ConfigurableJointMotion.Limited;
+        joint.angularXMotion = ConfigurableJointMotion.Limited;
+        joint.angularYMotion = ConfigurableJointMotion.Limited;
+        joint.angularZMotion = ConfigurableJointMotion.Limited;
+
+        var linearLimit = new SoftJointLimit() { limit = DistanceBetweenParticles * LinearLimitRelative };
+        joint.linearLimit = linearLimit;
+
+        var angularLimitNeg = new SoftJointLimit() { limit = -AngularLimit };
+        joint.lowAngularXLimit = angularLimitNeg;
+        var angularLimit = new SoftJointLimit() { limit = AngularLimit };
+        joint.highAngularXLimit = angularLimit;
+        joint.angularYLimit = angularLimit;
+        joint.angularZLimit = angularLimit;
+
+        var linearDrive = new JointDrive() { positionSpring = LinearSpring, positionDamper = LinearDamper, maximumForce = Mathf.Infinity };
+        joint.xDrive = linearDrive;
+
+        joint.rotationDriveMode = RotationDriveMode.Slerp;
+        var angularDrive = new JointDrive() { positionSpring = AngularSpring, positionDamper = AngularDamper, maximumForce = Mathf.Infinity };
+        joint.slerpDrive = angularDrive;
+
+        joint.breakForce = BreakForce;
+        joint.breakTorque = BreakTorque;
+
+        joint.projectionMode = JointProjectionMode.PositionAndRotation;
+        joint.projectionDistance = DistanceBetweenParticles * HardLinearLimitRelative;
+        joint.projectionAngle = HardAngularLimit;
+    }
+
+
+    /*
+    private void CopyJoint(ConfigurableJoint src, ConfigurableJoint dst)
+    {
+        // .......
+        Debug.LogError("NOT IMPLEMENTED!!!");
+    }
+    
+    
+
+    private ConfigurableJoint Connect(Rigidbody a, Rigidbody b)
+    {
+        if (!a || !b)
+            return null;
+
+        // Use the particle's joint, or dupplicate it if it's already in use.
+        var joint = a.gameObject.GetComponent<ConfigurableJoint>();
+        if (joint.connectedBody)
+        {
+            var oldJoint = joint;
+            joint = a.gameObject.AddComponent<ConfigurableJoint>();
+            CopyJoint(oldJoint, joint);
+        }
+        joint.connectedBody = b;
+
+        var dir = (b.position - a.position).normalized;
+        joint.axis = dir;
+        joint.secondaryAxis = Quaternion.LookRotation(dir) * Vector3.right;
+
+        Debug.Log(" QUE NO CHOQUEN ENTRE LAS PARTICULAS!! MOSTARAR UN MENSAJE DE ERROR SI OCURRE!!");
+
+        return joint;
+    }
+    */
 }
