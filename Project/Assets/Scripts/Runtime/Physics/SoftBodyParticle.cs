8e6d6ff445a9f200d3f81d08fb53edf5644ce69a
aurelioprovedo <aurelioprovedo@gmail.com>
2019-03-18 04:17:33 +0100

    Cleaning up messy trial-and-error code

--- Project/Assets/Scripts/Runtime/Physics/SoftBodyParticle.cs
+++ Project/Assets/Scripts/Runtime/Physics/SoftBodyParticle.cs
@@ -65,7 +65,7 @@ public class SoftBodyConnection
 [DisallowMultipleComponent]
 public class SoftBodyParticle : MonoBehaviour
 {
-    // TODO: not sure if the renderer should go in another component.
+    // TODO: refactor this class, renderer should go in another component.
 
     public SkinnedMeshRenderer SkinnedRenderer;
 
@@ -74,22 +74,8 @@ public class SoftBodyParticle : MonoBehaviour
     private HashSet<SoftBodyParticle> _connectedParticlesSet;
     public IEnumerable<SoftBodyParticle> ConnectedParticles { get => _connectedParticlesSet; }
 
-    //xpublic List<SoftBodyParticle> ConnectedParticles = new List<SoftBodyParticle>();
-    //x[SerializeField] /*[HideInInspector]*/ [ReadOnly] List<ConfigurableJoint> _jointsByConnectedParticles = new List<ConfigurableJoint>();
-
-    //xprivate List<SoftBodyParticle> _remoteConnections = new List<SoftBodyParticle>();
-
-
     public SoftBodyJointBreakEvent OnJointBroken;
 
-
-    // We need to cache the state of the Joints (alive/broken) because Unity nulls them after they break.
-    // This uses the same indices as the ConnectedParticles dictionary.
-    [SerializeField] [ReadOnly] private bool[] _previousJointStates;//y
-
-
-
-    //private Mesh _mesh;
 
     private Rigidbody _rb;
     public Rigidbody Rigidbody
@@ -113,58 +99,7 @@ public class SoftBodyParticle : MonoBehaviour
         if (OnJointBroken == null)
             OnJointBroken = new SoftBodyJointBreakEvent();
 
-        _previousJointStates = GetCurrentJointStates();//y
-
-        //_mesh = Instantiate<Mesh>(SkinnedRenderer.sharedMesh);
-        //_mesh.MarkDynamic();
-        //SkinnedRenderer.sharedMesh = _mesh;
-
-        // TESTING TO SEE IF IT FIXES THE WEIRD OFFSET WHEN REMOVING BONE WEIGHTS:
         SkinnedRenderer.rootBone = this.transform;
-
-        /*
-        //////////////////////////////////////////////////////////////////////////////////
-        // TEST:
-        // TRY REMOVING THE WEIGHTS FOR THE BONES THAT ARE NOT CONNECTED <-- THIS IS NOT GOING TO BE GOOD, YOU NEED TO PRESERVE THE JOINTS AS LONG AS POSSIBLE!
-        Debug.Log("TODO: RECORRER LA JERARQUIA DE CONEXIONES PARA VER SI AUN HAY ALGUNA CONEXION INDIRECTA.");
-        Debug.Log("TODO: ADEMÁS, NO TE BASES SOLO EN LOS ConnectedParticles LOCALES, LAS CONEXIONES SON RECÍPROCAS!");
-        foreach (var bone in SkinnedRenderer.bones)
-        {
-            if (bone != this.transform)
-            {
-                var isConnected = false;
-                foreach (var conn in ConnectedParticles)
-                {
-                    if (bone == conn.transform)
-                    {
-                        isConnected = true;
-                    }
-                }
-                if (!isConnected)
-                {
-                    DeactivateBoneByReplacingWithDummy(SkinnedRenderer, bone, this.transform);
-                }
-            }
-        }
-        // end TEST
-        //////////////////////////////////////////////////////////////////////////////////
-        ///*/
-
-        /*
-        this.OnJointBroken.AddListener(TryRemoveConnectedBoneFromSkin);
-        foreach (var boneTransform in SkinnedRenderer.bones)
-        {
-            var p = boneTransform.GetComponent<SoftBodyParticle>();
-            if (p)
-                p.OnJointBroken.AddListener(TryRemoveConnectedBoneFromSkin);
-        }
-        */
-    }
-
-
-    private void OnDestroy()
-    {
-        //DestroyImmediate(_mesh);
     }
 
 
@@ -191,10 +126,7 @@ public class SoftBodyParticle : MonoBehaviour
 
     public void Clear()
     {
-        //xRefreshJointsArraySize();
-
         // Delete any previously existing joint.
-        //xfor (int i = 0; i < _jointsByConnectedParticles.Count; i++)
         foreach (var conn in Connections)
         {
             if (conn.Joint == null)
@@ -202,57 +134,21 @@ public class SoftBodyParticle : MonoBehaviour
 
 #if UNITY_EDITOR
             if (!Application.isPlaying)
-                //xDestroyImmediate(_jointsByConnectedParticles[i]);
                 DestroyImmediate(conn.Joint);
             else
 #endif
-                //xDestroy(_jointsByConnectedParticles[i]);
                 Destroy(conn.Joint);
-
-            //x_jointsByConnectedParticles[i] = null;
-
-            //x_remoteConnections.Clear();
-
-            //xConnectedParticles[i].UnregisterRemoteJoint(this);
         }
-
-        //Connections.Clear();
     }
 
 
-    //xpublic void RegisterJoint(SoftBodyParticle other, ConfigurableJoint j)
-    //x{
-    //xRefreshJointsArraySize();
+    /* Leaving this here in case Unity decides to fix OnJointBreak()
+    private bool _isFindingBrokenJoint;
 
-    //xvar i = ConnectedParticles.IndexOf(other);
-    //x_jointsByConnectedParticles[i] = j;
-
-    //xother.RegisterRemoteJoint(this);
-    //x}
-
-
-
-    //x
-    //xprivate void RegisterRemoteJoint(SoftBodyParticle owner)
-    //x{
-    //xif (_remoteConnections.IndexOf(owner) == -1)
-    //x_remoteConnections.Add(owner);
-    //x}
-
-
-    //xprivate void UnregisterRemoteJoint(SoftBodyParticle owner)
-    //x{
-    //x_remoteConnections.Remove(owner);
-    //x}
-
-
-
-
-    //private IEnumerator OnJointBreak(float breakForce)
-    private void OnJointBreak(float breakForce)
+    private IEnumerator OnJointBreak(float breakForce)
     {
-        // As you can see, this callback does not include the reference to the Joint. Moreover, Joints are 
-        // destroyed by Unity after they break, but this happens asynchronously. Therefore, we need to keep track 
+        // As you can see, this callback does not receive the reference to the Joint that broke. Moreover, Joints 
+        // are destroyed by Unity after they break, but this happens asynchronously. Therefore, we need to keep track 
         // of the status of our joints, and keep polling them here until we find out that they have just been destroyed.
 
         // BIG CAVEAT: the 'breakForce' will sometimes not belong to the joint that we find. This is because
@@ -260,61 +156,35 @@ public class SoftBodyParticle : MonoBehaviour
         // to check the first joint that we detect as newly broken, we cannot be sure that the joint that
         // we find is the joint that Unity invoked this callback for.
 
-        //zStartCoroutine(FindBrokenJoint(breakForce));
-    }
-
-
-    private bool _isFindingBrokenJoint;
-
-    private IEnumerator FindBrokenJoint(float breakForce)
-    {
         // Force this coroutine to run one at a time.
         if (_isFindingBrokenJoint)
-        {
-            Debug.Log("aqui esperando...", this);
             yield return null;
-        }
         _isFindingBrokenJoint = true;
 
-        var statesOnBreakFrame = (bool[])_previousJointStates.Clone(); //y
         var wereAlreadyRegisteredAsBrokenAtBreakFrame = new bool[Connections.Count];
         for (int i = 0; i < wereAlreadyRegisteredAsBrokenAtBreakFrame.Length; i++)
             wereAlreadyRegisteredAsBrokenAtBreakFrame[i] = Connections[i].isAlreadyRegisteredAsBroken;
 
-        //UnityEditor.EditorApplication.isPaused = true;
-
         // As I said, we need to wait *at least* one frame to be sure that the joint is destroyed. Sigh...
         yield return null;
 
-        // TODO: modify the mesh (via shader distortion or vertexstreambuffer)
-
-        // NOTE: please Unity, just make this a proper callback and pass the Joint reference...
-
         // Find out which of the joints broke.
         var foundIndices = new List<int>();
-        //zvar foundIndex = -1;
         int iterations = 0;
         while (foundIndices.Count == 0)
-        //zwhile (foundIndex == -1)
         {
-            //// Reverse traversal for easy removal later if needed.
-            //for (int i = Connections.Count - 1; i >= 0; i--)
-            for (int i = 0; i < Connections.Count; i++)
-            //xfor (int i = 0; i < ConnectedParticles.Count; i++)
+            // Reverse traversal for easy removal later if needed.
+            for (int i = Connections.Count - 1; i >= 0; i--)
             {
-                //xvar j = _jointsByConnectedParticles[i];
                 var conn = Connections[i];
-                var jointState = !IsJointBroken(conn.Joint);//y
-                if (!jointState && statesOnBreakFrame[i]) //y
-                //yif (conn.isBroken && !wereAlreadyRegisteredAsBrokenAtBreakFrame[i])
+                var jointState = !IsJointBroken(conn.Joint);
+                if (conn.isBroken && !wereAlreadyRegisteredAsBrokenAtBreakFrame[i])
                 {
                     foundIndices.Add(i);
-                    //zfoundIndex = i;
                     conn.isAlreadyRegisteredAsBroken = true;
                 }
             }
             if (foundIndices.Count == 0)
-            //zif (foundIndex == -1)
                 yield return null;
 
             iterations++;
@@ -324,44 +194,33 @@ public class SoftBodyParticle : MonoBehaviour
 
         foreach (var brokenConnectionIndex in foundIndices)
         {
-            //xvar p = ConnectedParticles[i];
             var conn = Connections[brokenConnectionIndex];
-            //zvar conn = Connections[foundIndex];
 
             //Debug.Log("Broken joint from <b>" + conn.OwnerParticle.name + "</b> to <b>" + conn.ConnectedParticle.name + "</b>, breakForce <b>" + breakForce.ToString() + "</b>", this);
 
             // So much work... just for sending what should have been passed as an event argument... -_-
-            //xOnJointBroken.Invoke(new SoftBodyJointBreakEventData(this, p, breakForce, _jointsByConnectedParticles[brokenConnectionIndex]));
             OnJointBroken.Invoke(new SoftBodyJointBreakEventData(conn.OwnerParticle, conn.ConnectedParticle, breakForce, conn.Joint));
         }
-
-        _previousJointStates = GetCurrentJointStates();//y
 
         // Allow a next coroutine to keep working.
         _isFindingBrokenJoint = false;
     }
+    */
 
 
     private void FixedUpdate()
     {
-        // INSTEAD OF LISTENING FOR BROKEN JOINTS, JUST QUERY THEM EVERY FRAME.
-
         float fakeBreakForce = -1;
 
         var wereAlreadyRegisteredAsBrokenAtBreakFrame = new bool[Connections.Count];
         for (int i = 0; i < wereAlreadyRegisteredAsBrokenAtBreakFrame.Length; i++)
             wereAlreadyRegisteredAsBrokenAtBreakFrame[i] = Connections[i].isAlreadyRegisteredAsBroken;
 
-        // TODO: modify the mesh (via shader distortion or vertexstreambuffer)
-
-        // NOTE: please Unity, just make this a proper callback and pass the Joint reference...
-
         // Find out which of the joints broke.
         var foundIndices = new List<int>();
             
-        //// Reverse traversal for easy removal later if needed.
-        //for (int i = Connections.Count - 1; i >= 0; i--)
-        for (int i = 0; i < Connections.Count; i++)
+        // Reverse traversal for easy removal later if needed.
+        for (int i = Connections.Count - 1; i >= 0; i--)
         {
             var conn = Connections[i];
             if (conn.isBroken && !wereAlreadyRegisteredAsBrokenAtBreakFrame[i])
@@ -382,37 +241,12 @@ public class SoftBodyParticle : MonoBehaviour
         }
     }
 
-    private bool[] GetCurrentJointStates()//y
-    {
-        //yvar states = new bool[ConnectedParticles.Count];
-        var states = new bool[Connections.Count];
-        //yfor (int i = 0; i < ConnectedParticles.Count; i++)
-        for (int i = 0; i < Connections.Count; i++)
-            //ystates[i] = !IsJointBroken(_jointsByConnectedParticles[i]);
-            states[i] = !IsJointBroken(Connections[i].Joint);
-        return states;
-    }
-
-
-    //xprivate void OnValidate()
-    //x{
-    //xRefreshJointsArraySize();
-    //x}
-
 
     private void OnValidate()
     {
         foreach (var conn in Connections)
             conn.OwnerParticle = this;
     }
-
-    //xprivate void RefreshJointsArraySize()
-    //x{
-    //xwhile (_jointsByConnectedParticles.Count > ConnectedParticles.Count)
-    //x_jointsByConnectedParticles.RemoveAt(_jointsByConnectedParticles.Count - 1);
-    //xwhile (_jointsByConnectedParticles.Count != ConnectedParticles.Count)
-    //x_jointsByConnectedParticles.Add(null);
-    //x}
 
 
 #if UNITY_EDITOR
@@ -432,7 +266,6 @@ public class SoftBodyParticle : MonoBehaviour
 
         var pos = transform.position;
 
-        //xfor (int i = 0; i < ConnectedParticles.Count; i++)
         foreach (var conn in Connections)
         {
             if (!conn.ConnectedParticle)
@@ -442,7 +275,6 @@ public class SoftBodyParticle : MonoBehaviour
             var dir = AtoB.normalized;
             var dist = Vector3.Dot(AtoB, dir);
 
-            //xHandles.color = _jointsByConnectedParticles[i] ? connectedColor : Handles.xAxisColor;
             Handles.color = conn.isBroken ? brokenColor : connectedColor;
             Handles.ArrowHandleCap(0, pos, Quaternion.LookRotation(dir), dist*.85f, EventType.Repaint);
         }
--- Project/Assets/_WIP/Scripts/TEST_SoftBodyBreakerByDeactivatingBones.cs
+++ Project/Assets/_WIP/Scripts/TEST_SoftBodyBreakerByDeactivatingBones.cs
@@ -4,11 +4,14 @@
 [RequireComponent(typeof(SoftBody))]
 public class TEST_SoftBodyBreakerByDeactivatingBones : MonoBehaviour
 {
-    private readonly float kDisconnectionCheckFrequency = .1f;
+    [Min(.01f)] public float DisconnectedBonesCheckFrequency = .1f;
 
     private SoftBody _softBody;
 
     private float _nextDisconnectionCheckTime;
+
+
+    // TODO: refactor this class, renderer should go in another component.
 
 
     void Start()
@@ -18,7 +21,7 @@ public class TEST_SoftBodyBreakerByDeactivatingBones : MonoBehaviour
         foreach (var p in _softBody.Particles)
             p.OnJointBroken.AddListener(OnSoftBodyJointBroken);
 
-        _nextDisconnectionCheckTime = Time.time + kDisconnectionCheckFrequency;
+        _nextDisconnectionCheckTime = Time.time + DisconnectedBonesCheckFrequency;
     }
 
 
@@ -29,7 +32,7 @@ public class TEST_SoftBodyBreakerByDeactivatingBones : MonoBehaviour
         if (Time.time < _nextDisconnectionCheckTime)
             return;
 
-        _nextDisconnectionCheckTime = _nextDisconnectionCheckTime + kDisconnectionCheckFrequency;
+        _nextDisconnectionCheckTime = Time.time + DisconnectedBonesCheckFrequency;
 
         // Loop the bones in the particles' skinnedMeshRenderers, and filter out the bones that are not particles of the soft body.
         // This will early-out all the dummy bones that we may have already created.
@@ -134,68 +137,5 @@ public class TEST_SoftBodyBreakerByDeactivatingBones : MonoBehaviour
 
         return removed;
     }
-
-
-    #region OLD & POSSIBLY DEPRECATED
-    // Returns false if the bone is not influencing the skin.
-    public static bool RemoveSkinBone_OLD_APPROACH__REMOVING_MESH_BONEWEIGHTS(SkinnedMeshRenderer skin, Transform bone)
-    {
-        var removed = false;
-
-        for (int i = 0; !removed && i < skin.bones.Length; i++)
-        {
-            if (skin.bones[i] == bone)
-            {
-                skin.bones[i] = null;
-                RemoveMeshBoneWeights(skin.sharedMesh, i);
-                removed = true;
-            }
-        }
-
-        return removed;
-    }
-
-    public static void RemoveMeshBoneWeights(Mesh mesh, int boneIndex)
-    {
-        var newBoneWeights = mesh.boneWeights;
-        for (int vertexIndex = 0; vertexIndex < newBoneWeights.Length; vertexIndex++)
-        {
-            if (newBoneWeights[vertexIndex].boneIndex0 == boneIndex)
-            {
-                newBoneWeights[vertexIndex].weight0 = 0;
-                var norm = new Vector3(newBoneWeights[vertexIndex].weight1, newBoneWeights[vertexIndex].weight2, newBoneWeights[vertexIndex].weight3).normalized;
-                newBoneWeights[vertexIndex].weight1 = norm.x;
-                newBoneWeights[vertexIndex].weight2 = norm.y;
-                newBoneWeights[vertexIndex].weight3 = norm.z;
-            }
-            else if (newBoneWeights[vertexIndex].boneIndex1 == boneIndex)
-            {
-                newBoneWeights[vertexIndex].weight1 = 0;
-                var norm = new Vector3(newBoneWeights[vertexIndex].weight0, newBoneWeights[vertexIndex].weight2, newBoneWeights[vertexIndex].weight3).normalized;
-                newBoneWeights[vertexIndex].weight0 = norm.x;
-                newBoneWeights[vertexIndex].weight2 = norm.y;
-                newBoneWeights[vertexIndex].weight3 = norm.z;
-            }
-            else if (newBoneWeights[vertexIndex].boneIndex2 == boneIndex)
-            {
-                newBoneWeights[vertexIndex].weight2 = 0;
-                var norm = new Vector3(newBoneWeights[vertexIndex].weight0, newBoneWeights[vertexIndex].weight1, newBoneWeights[vertexIndex].weight3).normalized;
-                newBoneWeights[vertexIndex].weight0 = norm.x;
-                newBoneWeights[vertexIndex].weight1 = norm.y;
-                newBoneWeights[vertexIndex].weight3 = norm.z;
-            }
-            else
-            {
-                newBoneWeights[vertexIndex].weight3 = 0;
-                var norm = new Vector3(newBoneWeights[vertexIndex].weight0, newBoneWeights[vertexIndex].weight1, newBoneWeights[vertexIndex].weight2).normalized;
-                newBoneWeights[vertexIndex].weight0 = norm.x;
-                newBoneWeights[vertexIndex].weight1 = norm.y;
-                newBoneWeights[vertexIndex].weight2 = norm.z;
-            }
-        }
-
-        mesh.boneWeights = newBoneWeights;
-    }
-    #endregion OLD & POSSIBLY DEPRECATED
 }
 
